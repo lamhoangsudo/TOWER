91117b16a34027d059a7dfbbc12a064430a56bdc
lamhoangsudo <hlam5370@gmail.com>
2025-06-27 14:57:47 +0700

    Add ProjecTileMovementSystem for projectile ECS logic
    Introduced ProjecTileMovementSystem to handle projectile movement, lifetime reduction, and entity destruction in Unity DOTS. Updated Plastic SCM branch to 'main/VattalusProjectileECSConvert'.

---
+++ Assets/Script/DOTS/SYSTEM/ProjecTileMovementSystem.cs
@@ -0,0 +1,42 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+
+partial struct ProjecTileMovementSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        float deltaTime = SystemAPI.Time.DeltaTime;
+
+        foreach ((RefRW<ProjecTile> projectile, RefRW<LocalTransform> transform, Entity entity) in
+                 SystemAPI.Query<RefRW<ProjecTile>, RefRW<LocalTransform>>()
+                 .WithEntityAccess())
+        {
+            // Di chuyển theo hướng
+            transform.ValueRW.Position +=
+                projectile.ValueRO.direction * projectile.ValueRO.speed * deltaTime;
+
+            // Giảm lifetime
+            projectile.ValueRW.lifetime -= deltaTime;
+
+            // Hết thời gian sống thì destroy
+            if (projectile.ValueRW.lifetime <= 0f)
+            {
+                state.EntityManager.DestroyEntity(entity);
+            }
+        }
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+
+    }
+}
